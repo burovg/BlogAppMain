6d375662e3b048eeda3cb1fe0232c5726b44bc5a
gershon <gershon@cio.gov.il>
2018-06-07 18:03:13 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+
+
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import PostIndex from './components/post_index';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={PostIndex} />
+      </Switch>
+    </BrowserRouter>
+    </Provider>
+    , document.getElementById('root')
+);
 registerServiceWorker();
