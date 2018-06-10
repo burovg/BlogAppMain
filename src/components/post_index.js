149215e03d77ecda155e7bf466360b902ebf9719
gershon <gershon@cio.gov.il>
2018-06-10 16:07:52 +0300

    3

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -3,6 +3,11 @@ import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
 
 class PostIndex extends Component{
+
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
+
     render(){
         return(
             <div>Post Index</div>
