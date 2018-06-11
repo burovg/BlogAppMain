8210a3cd9373a280e5137aa51212a59a29a1b8a1
gershon <gershon@cio.gov.il>
2018-06-11 16:58:02 +0300

    4

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,11 +1,46 @@
 import React,{Component} from 'react';
+import {Field,reduxForm} from 'redux-form';
 
 class NewPost extends Component{
+
+    renderField(field){
+        return(
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input 
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
     render(){
         return(
-            <div>New Post</div>
+            <form>
+                <Field 
+                    name="title"
+                    label="Title:"
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Tags:"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Post content:"
+                    name="content"
+                    component={this.renderField}
+                />
+            </form>
         );
     }
 }
 
-export default NewPost;
+export default reduxForm(
+{
+    form:'PostNewForm'
+}
+)(NewPost);
